eab7c89b0601f4d308d0f20746c1a52550a3f44e
Roman Andrushchenko <rom.andrushchenko@gmail.com>
2024-12-11 14:19:42 +0200

    additional operators stateMap and combineStates for StateFlow

--- container/src/main/java/com/elveum/container/StateFlowExtensions.kt
+++ container/src/main/java/com/elveum/container/StateFlowExtensions.kt
@@ -1,8 +1,15 @@
 package com.elveum.container
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 /**
  * Cast [MutableStateFlow] to [StateFlow].
@@ -26,5 +33,174 @@ public fun <T> StateFlow<T>.tryUpdate(value: T) {
 public inline fun <T> StateFlow<T>.tryUpdate(updater: (T) -> T) {
     if (this is MutableStateFlow) {
         update(updater)
+    }
+}
+
+/**
+ * Almost the same operator as [Flow.map] but it returns [StateFlow] instance
+ * instead of [Flow].
+ */
+public fun <T, R> StateFlow<T>.stateMap(mapper: (T) -> R): StateFlow<R> {
+    return MapStateFlow(this, mapper)
+}
+
+/**
+ * Almost the same operator as [combine] but it returns [StateFlow] instance
+ * instead of [Flow].
+ */
+public fun <T1, T2, R> combineStates(
+    stateFlow1: StateFlow<T1>,
+    stateFlow2: StateFlow<T2>,
+    transform: (T1, T2) -> R,
+): StateFlow<R> {
+    return combineStates(listOf(stateFlow1, stateFlow2)) { input ->
+        @Suppress("UNCHECKED_CAST")
+        transform(input[0] as T1, input[1] as T2)
+    }
+
+}
+
+/**
+ * Almost the same operator as [combine] but it returns [StateFlow] instance
+ * instead of [Flow].
+ */
+public fun <T1, T2, T3, R> combineStates(
+    stateFlow1: StateFlow<T1>,
+    stateFlow2: StateFlow<T2>,
+    stateFlow3: StateFlow<T3>,
+    transform: (T1, T2, T3) -> R,
+): StateFlow<R> {
+    return combineStates(listOf(stateFlow1, stateFlow2, stateFlow3)) { input ->
+        @Suppress("UNCHECKED_CAST")
+        transform(input[0] as T1, input[1] as T2, input[2] as T3)
+    }
+
+}
+
+/**
+ * Almost the same operator as [combine] but it returns [StateFlow] instance
+ * instead of [Flow].
+ */
+public fun <T1, T2, T3, T4, R> combineStates(
+    stateFlow1: StateFlow<T1>,
+    stateFlow2: StateFlow<T2>,
+    stateFlow3: StateFlow<T3>,
+    stateFlow4: StateFlow<T4>,
+    transform: (T1, T2, T3, T4) -> R,
+): StateFlow<R> {
+    return combineStates(listOf(stateFlow1, stateFlow2, stateFlow3, stateFlow4)) { input ->
+        @Suppress("UNCHECKED_CAST")
+        transform(input[0] as T1, input[1] as T2, input[2] as T3, input[3] as T4)
+    }
+}
+
+/**
+ * Almost the same operator as [combine] but it returns [StateFlow] instance
+ * instead of [Flow].
+ */
+public fun <T1, T2, T3, T4, T5, R> combineStates(
+    stateFlow1: StateFlow<T1>,
+    stateFlow2: StateFlow<T2>,
+    stateFlow3: StateFlow<T3>,
+    stateFlow4: StateFlow<T4>,
+    stateFlow5: StateFlow<T5>,
+    transform: (T1, T2, T3, T4, T5) -> R,
+): StateFlow<R> {
+    return combineStates(listOf(stateFlow1, stateFlow2, stateFlow3, stateFlow4, stateFlow5)) { input ->
+        @Suppress("UNCHECKED_CAST")
+        transform(input[0] as T1, input[1] as T2, input[2] as T3, input[3] as T4, input[4] as T5)
+    }
+}
+
+/**
+ * Combine multiple state-flows into one.
+ */
+public fun <R> combineStates(
+    stateFlows: Iterable<StateFlow<*>>,
+    transform: (List<*>) -> R,
+): StateFlow<R> {
+    return CombineStateFlow(
+        flows = stateFlows,
+        transform = transform,
+    )
+}
+
+internal class MapStateFlow<T, R>(
+    private val originFlow: StateFlow<T>,
+    mapper: (T) -> R,
+) : StateFlow<R> {
+
+    private val cachedCalculation = CachedCalculation(originFlow.value, mapper)
+
+    override val replayCache: List<R>
+        get() = listOf(value)
+    override val value: R
+        get() = cachedCalculation.calculate(originFlow.value)
+
+    override suspend fun collect(collector: FlowCollector<R>): Nothing {
+        originFlow.collect {
+            collector.emit(cachedCalculation.calculate(it))
+        }
+    }
+}
+
+internal class CombineStateFlow<R>(
+    private val flows: Iterable<StateFlow<*>>,
+    transform: (List<*>) -> R,
+) : StateFlow<R> {
+
+    private val currentInputs get() = flows.map { it.value }
+    private val cachedCalculation = CachedCalculation(currentInputs, transform)
+
+    override val replayCache: List<R>
+        get() = listOf(value)
+    override val value: R
+        get() = cachedCalculation.calculate(currentInputs)
+
+    override suspend fun collect(collector: FlowCollector<R>): Nothing {
+        while (true) {
+            coroutineScope {
+                collector.emit(value)
+                flows.forEachIndexed { currentFlowIndex, currentStateFlow ->
+                    launch {
+                        currentStateFlow
+                            .drop(1)
+                            .collect { item ->
+                                val inputs = flows.mapIndexed { index, stateFlow ->
+                                    if (index == currentFlowIndex) {
+                                        item
+                                    } else {
+                                        stateFlow.value
+                                    }
+                                }
+                                val transformedValue = cachedCalculation.calculate(inputs)
+                                collector.emit(transformedValue)
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+internal class CachedCalculation<Input, T>(
+    initialInput: Input,
+    private val calcFunction: (Input) -> T,
+) {
+    private var lastInput: Input = initialInput
+    private var cachedResult: Container<T> = Container.Pending
+
+    fun calculate(input: Input): T = synchronized(this) {
+        val lastInput = this.lastInput
+        val cachedResult = this.cachedResult
+        if (cachedResult is Container.Success && lastInput == input) {
+            cachedResult.value
+        } else {
+            calcFunction(input).also {
+                this.lastInput = input
+                this.cachedResult = Container.Success(it)
+            }
+        }
     }
 }
--- container/src/test/java/com/elveum/container/StateFlowExtensionsTest.kt
+++ container/src/test/java/com/elveum/container/StateFlowExtensionsTest.kt
@@ -1,5 +1,6 @@
 package com.elveum.container
 
+import com.elveum.container.utils.runFlowTest
 import io.mockk.called
 import io.mockk.every
 import io.mockk.mockk
@@ -59,6 +60,193 @@ class StateFlowExtensionsTest {
         verify(exactly = 1) {
             updater wasNot called
         }
+    }
+
+    @Test
+    fun test_stateMap() = runFlowTest {
+        val originFlow = MutableStateFlow(1)
+        val mapFunction: (Int) -> String = mockk()
+        every { mapFunction.invoke(any()) } answers {
+            "item${firstArg<Int>()}"
+        }
+        val mappedFlow = originFlow.stateMap(mapFunction)
+
+        val collectedItems = mappedFlow.startCollectingToList()
+
+        // initial state
+        assertEquals(1, collectedItems.size)
+        assertEquals("item1", collectedItems.last())
+        assertEquals("item1", mappedFlow.value)
+        assertEquals(listOf("item1"), mappedFlow.replayCache)
+        verify(exactly = 1) {
+            mapFunction(1)
+        }
+        // emit a new value
+        originFlow.value = 2
+        assertEquals(2, collectedItems.size)
+        assertEquals("item2", collectedItems.last())
+        assertEquals("item2", mappedFlow.value)
+        assertEquals(listOf("item2"), mappedFlow.replayCache)
+        verify(exactly = 1) {
+            mapFunction(2)
+        }
+        // emit the same value
+        originFlow.value = 2
+        assertEquals(2, collectedItems.size)
+        assertEquals("item2", collectedItems.last())
+        assertEquals("item2", mappedFlow.value)
+        assertEquals(listOf("item2"), mappedFlow.replayCache)
+        verify(exactly = 1) {
+            mapFunction(2)
+        }
+        // emit a new value again
+        originFlow.value = 3
+        assertEquals(3, collectedItems.size)
+        assertEquals("item3", collectedItems.last())
+        assertEquals("item3", mappedFlow.value)
+        assertEquals(listOf("item3"), mappedFlow.replayCache)
+        verify(exactly = 1) {
+            mapFunction(3)
+        }
+    }
+
+    @Test
+    fun test_combineStates() = runFlowTest {
+        val flowA = MutableStateFlow("a1")
+        val flowB = MutableStateFlow("b1")
+        val flowC = MutableStateFlow("c1")
+        val combineFunction: (List<*>) -> String = mockk()
+        every { combineFunction.invoke(any()) } answers {
+            val inputs = firstArg<List<String>>()
+            inputs.joinToString("-")
+        }
+
+        val combinedFlow = combineStates(listOf(flowA, flowB, flowC), transform = combineFunction)
+        val collectedItems = combinedFlow.startCollectingToList()
+
+        // initial state
+        assertEquals(1, collectedItems.size)
+        assertEquals("a1-b1-c1", collectedItems.last())
+        assertEquals("a1-b1-c1", combinedFlow.value)
+        verify(exactly = 1) {
+            combineFunction(listOf("a1", "b1", "c1"))
+        }
+        // emit the same values
+        flowA.value = "a1"
+        flowB.value = "b1"
+        flowC.value = "c1"
+        assertEquals(1, collectedItems.size)
+        assertEquals("a1-b1-c1", collectedItems.last())
+        assertEquals("a1-b1-c1", combinedFlow.value)
+        verify(exactly = 1) {
+            combineFunction(listOf("a1", "b1", "c1"))
+        }
+        // emit 1 new value
+        flowB.value = "b2"
+        assertEquals(2, collectedItems.size)
+        assertEquals("a1-b2-c1", collectedItems.last())
+        assertEquals("a1-b2-c1", combinedFlow.value)
+        verify(exactly = 1) {
+            combineFunction(listOf("a1", "b2", "c1"))
+        }
+        // emit 2 new values
+        flowB.value = "b3"
+        flowC.value = "c2"
+        assertEquals(4, collectedItems.size)
+        assertEquals("a1-b3-c1", collectedItems[collectedItems.lastIndex - 1])
+        assertEquals("a1-b3-c2", collectedItems[collectedItems.lastIndex])
+        assertEquals("a1-b3-c2", combinedFlow.value)
+        verify(exactly = 1) {
+            combineFunction(listOf("a1", "b3", "c1"))
+            combineFunction(listOf("a1", "b3", "c2"))
+        }
+    }
+
+    @Test
+    fun test_combine2states() {
+        val flowA = MutableStateFlow("a1")
+        val flowB = MutableStateFlow("b1")
+
+        val combinedFlow = combineStates(flowA, flowB) { a, b -> "$a-$b" }
+
+        // initial state
+        assertEquals("a1-b1", combinedFlow.value)
+        // emit new values
+        flowA.value = "a2"
+        assertEquals("a2-b1", combinedFlow.value)
+        flowB.value = "b2"
+        assertEquals("a2-b2", combinedFlow.value)
+    }
+
+    @Test
+    fun test_combine3states() {
+        val flowA = MutableStateFlow("a1")
+        val flowB = MutableStateFlow("b1")
+        val flowC = MutableStateFlow("c1")
+
+        val combinedFlow = combineStates(flowA, flowB, flowC) { a, b, c -> "$a-$b-$c" }
+
+        // initial state
+        assertEquals("a1-b1-c1", combinedFlow.value)
+        // emit new values
+        flowA.value = "a2"
+        assertEquals("a2-b1-c1", combinedFlow.value)
+        flowB.value = "b2"
+        assertEquals("a2-b2-c1", combinedFlow.value)
+        flowC.value = "c2"
+        assertEquals("a2-b2-c2", combinedFlow.value)
+    }
+
+
+    @Test
+    fun test_combine4states() {
+        val flowA = MutableStateFlow("a1")
+        val flowB = MutableStateFlow("b1")
+        val flowC = MutableStateFlow("c1")
+        val flowD = MutableStateFlow("d1")
+
+        val combinedFlow = combineStates(flowA, flowB, flowC, flowD) { a, b, c, d ->
+            "$a-$b-$c-$d"
+        }
+
+        // initial state
+        assertEquals("a1-b1-c1-d1", combinedFlow.value)
+        // emit new values
+        flowA.value = "a2"
+        assertEquals("a2-b1-c1-d1", combinedFlow.value)
+        flowB.value = "b2"
+        assertEquals("a2-b2-c1-d1", combinedFlow.value)
+        flowC.value = "c2"
+        assertEquals("a2-b2-c2-d1", combinedFlow.value)
+        flowD.value = "d2"
+        assertEquals("a2-b2-c2-d2", combinedFlow.value)
+    }
+
+    @Test
+    fun test_combine5states() {
+        val flowA = MutableStateFlow("a1")
+        val flowB = MutableStateFlow("b1")
+        val flowC = MutableStateFlow("c1")
+        val flowD = MutableStateFlow("d1")
+        val flowE = MutableStateFlow("e1")
+
+        val combinedFlow = combineStates(flowA, flowB, flowC, flowD, flowE) { a, b, c, d, e ->
+            "$a-$b-$c-$d-$e"
+        }
+
+        // initial state
+        assertEquals("a1-b1-c1-d1-e1", combinedFlow.value)
+        // emit new values
+        flowA.value = "a2"
+        assertEquals("a2-b1-c1-d1-e1", combinedFlow.value)
+        flowB.value = "b2"
+        assertEquals("a2-b2-c1-d1-e1", combinedFlow.value)
+        flowC.value = "c2"
+        assertEquals("a2-b2-c2-d1-e1", combinedFlow.value)
+        flowD.value = "d2"
+        assertEquals("a2-b2-c2-d2-e1", combinedFlow.value)
+        flowE.value = "e2"
+        assertEquals("a2-b2-c2-d2-e2", combinedFlow.value)
     }
 
 }

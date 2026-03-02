ad2a4160da0ccd6cad0f2ff83aa294f22d414d3d
Roman Andrushchenko <rom.andrushchenko@gmail.com>
2026-03-02 21:36:15 +0200

    Design simple page loaders

---
+++ container/src/main/java/com/elveum/container/subject/paging/PageLoader.kt
@@ -0,0 +1,75 @@
+package com.elveum.container.subject.paging
+
+import com.elveum.container.subject.LazyFlowSubject
+import com.elveum.container.subject.StatefulValueLoader
+import com.elveum.container.subject.ValueLoader
+import com.elveum.container.subject.paging.internal.PageLoaderImpl
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Create a new [PageLoader] that can be assigned as a [ValueLoader] when instantiating
+ * a new [LazyFlowSubject].
+ *
+ * Page loader can load multiple pages on demand:
+ *
+ * ```
+ * val itemsPageLoader = pageLoader(initialKey = 0) { index ->
+ *     val items = source.fetchItems(pageIndex = index, pageSize = 20)
+ *     emitPage(items) // emit loaded items of the current page
+ *     emitNextKey(index + 1) // emit a key for the next page to be loaded
+ * }
+ *
+ * val subject = LazyFlowSubject.create(
+ *     valueLoader = itemsPageLoader
+ * )
+ * ```
+ *
+ * The `emitPage` can be called multiple times. For example, you can load
+ * data from a local storage at first, and after that - from a remote store.
+ *
+ * The `emitNextKey` can be called either 1 time (when there is a next page
+ * available), or 0 time (if the current page is the last one).
+ */
+public fun <Key, T> pageLoader(
+    initialKey: Key,
+    block: suspend PageEmitter<Key, T>.(Key) -> Unit
+): PageLoader<Key, T> {
+    return PageLoaderImpl(
+        initialKey = initialKey,
+        block = block,
+    )
+}
+
+/**
+ * A loader for fetching multiple pages of data. It can be created by using [pageLoader]
+ * function.
+ */
+public interface PageLoader<Key, T> : StatefulValueLoader<List<T>> {
+
+    /**
+     * Listen the next page loading state.
+     *
+     * @see PageState
+     */
+    public val nextPageState: StateFlow<PageState>
+
+    /**
+     * Call this method when the item with the specified [index] has been rendered.
+     *
+     * This call notifies the loader to start loading a next page if needed. For example,
+     * you can call it as a side effect within `LazyColumn`:
+     *
+     * ```
+     * LazyColumn {
+     *     itemsIndexed(list) { index, item ->
+     *         SideEffect {
+     *             pageLoader.onItemRendered(index)
+     *         }
+     *         Item(item)
+     *     }
+     * }
+     * ```
+     */
+    public fun onItemRendered(index: Int)
+
+}
